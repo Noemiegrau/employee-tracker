c520a108142b2e7a2184542ff0fe0f5ab7f5d2a5
Noemiegrau <noemiegrau@outlook.fr>
2021-05-06 16:29:23 -0700

    added routes folders

---
+++ db/connection.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2');
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: '19969696NOo',
+      database: 'employeeTracker'
+    },
+    console.log('Connected to the election database.')
+);
+
+  module.exports = db;
